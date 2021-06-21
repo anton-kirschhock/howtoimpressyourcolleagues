4fb163dddaeb7c98fcab06937fde2c031a6dfea8
Anton Kirschock <anton_wooddog@hotmail.com>
2021-06-21 19:39:08 +0200

    Preventing Development config from being available in GitHub

--- src/Kirschhock.HTIYC/Program.cs
+++ src/Kirschhock.HTIYC/Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Kirschhock.HTIYC
 {
@@ -19,6 +14,16 @@ namespace Kirschhock.HTIYC
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile("appsettings.json", false);
+
+#if DEBUG
+                builder.AddJsonFile("appsettings.Development.json", true);
+#endif
+
+                builder.AddEnvironmentVariables();
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
